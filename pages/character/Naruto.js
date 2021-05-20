b1b7f0279cb113f4f2e32b6545d4dc0c17cf3e9c
Alexander Briones <alexanderbvart@gmail.com>
2021-05-20 03:06:49 -0500

    |(feat:import)     arrange import of the next head

--- pages/character/Naruto.js
+++ pages/character/Naruto.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head'
+import Head from 'next/head'
 import Hero from '../../components/Hero';
 import SectionWrapper from '../../components/SectionWrapper/index,';
 import {naruto} from "./dataNaruto";
@@ -7,7 +7,11 @@ const Naruto = () => {
 
     return ( 
         <>
-
+            <Head>
+                <title>Naruto</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="theme-color" content="#15171B" />
+            </Head>
             <Hero header={naruto.header} />
             <SectionWrapper content={naruto}></SectionWrapper>
         </>
