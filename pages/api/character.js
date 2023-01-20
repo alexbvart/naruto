dae511f90262f4cab7de14a79a91ec1c479f54fd
alexbvart <alexanderbvart@gmail.com>
2023-01-20 07:51:03 -0500

    fix character endpoint

--- pages/api/character.js
+++ pages/api/character.js
@@ -6,8 +6,8 @@ const getCharacter = async (req, res) => {
     
     const jsonDirectory = path.join(process.cwd(), 'json');
     const fileContents = await fs.readFile(jsonDirectory + '/character.json', 'utf8');
-    // const json = JSON.parse(file)
+    const json = JSON.parse(fileContents)
 
-    res.status(200).json(fileContents)
+    res.status(200).json(json)
 }
 export default getCharacter;
