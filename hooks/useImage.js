9b9dbf97551c16c80f1646cc0f54222fc4c525da
alexbvart <alexanderbvart@gmail.com>
2023-01-22 04:09:23 -0500

    less page character

--- hooks/useImage.js
+++ hooks/useImage.js
@@ -1,5 +1,4 @@
 export const useImage = ({src, min='18', max='600'}) => {
-
     const regex = /\/scale-to-width-down\/[0-9]+\?/g;
     const lowQuality = src.replace(regex,`/scale-to-width-down/${min}?`)
     const highQuality = src.replace(regex,`/scale-to-width-down/${max}?`)
